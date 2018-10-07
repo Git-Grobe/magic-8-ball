132c0558ec77ede464616d9c1e60e9654733c07b
Scott Grobe <gitgrobe@gmail.com>
2018-10-07 03:29:55 -0400

    update
    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   var magic8Ball = {};
-  magic8Ball.listofanswers = ["Don't fucking count on it"];
+  magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 
   magic8Ball.getAnswer = function(question)
   {
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $("#8ball").effect( "shake" );
     $("#answer").text( answer );
     $("#answer").fadeIn(3000);
-    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+    $("#8ball").attr("src", "magic8ballAnswer-mod.png");
 
     console.log(question);
     console.log(answer);
