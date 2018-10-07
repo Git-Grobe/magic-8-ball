0574bb5d2af6bb08d4f26115d05ad54b271ce6d0
Scott Grobe <gitgrobe@gmail.com>
2018-10-07 03:32:17 -0400

    update
    update

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
   var onClick = function()
   {
     $("#answer").hide();
-    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+    $("#8ball").attr("src", "magic8ballAnswer-mod.png");
     var question = prompt("What do you want to know?");
     magic8Ball.getAnswer(question);
   };
